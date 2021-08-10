1c0c6248d95f5a3b45b96620be47d81ab6a080b7
FabianLars <fabianlars@fabianlars.de>
2021-08-10 13:52:44 +0200

    init

---
+++ README.md
@@ -0,0 +1,62 @@
+# minimal-tauri-cli
+
+A minimal wrapper for a prebuilt version of tauri's cli, without any dependencies. Initially created for CI/CD to reduce installation time and size of the node_modules/ folder.
+
+Downloads the cli from https://github.com/tauri-apps/binary-releases and forwards any calls like `npm tauri dev` to the binary.
+
+## About Tauri
+
+See: https://tauri.studio and https://github.com/tauri-apps/tauri
+
+# Installation
+
+This package is meant to be installed locally as a development dependency:
+
+```
+$ npm install --save-dev minimal-tauri-cli
+$ yarn add --dev minimal-tauri-cli
+$ pnpm install --dev minimal-tauri-cli
+```
+
+# Usage
+
+## Example syntax:
+
+```
+$ pnpm tauri dev
+```
+
+## To forward arguments like `--debug` you might need to prefix them with an extra `--`:
+
+```
+$ pnpm tauri build -- --debug
+```
+
+## `tauri help`:
+
+```
+$ pnpm tauri help
+
+USAGE:
+    cargo tauri [SUBCOMMAND]
+
+FLAGS:
+    -h, --help       Prints help information
+    -V, --version    Prints version information
+
+SUBCOMMANDS:
+    build    Tauri build.
+    dev      Tauri dev.
+    help     Prints this message or the help of the given subcommand(s)
+    info     Shows information about Tauri dependencies
+    init     Initializes a Tauri project
+    sign     Tauri updates signer.
+```
+
+# Warnings:
+
+-   Conflicts with `@tauri-apps/cli`.
+-   Might have version mismatches with `@tauri-apps/cli`, because `minimal-tauri-cli`'s version is synced with its rust-based counterpart, see: https://github.com/tauri-apps/tauri/tree/dev/tooling/cli.rs
+-   Doesn't support `tauri icon` and `tauri deps` as long as they are not provided by tauri's rust-based cli.
+-   There is only a bare minimum of error handling implemented.
+-   The download function is pretty primitive and might fail on complex network setups.
---
+++ index.js
@@ -0,0 +1,84 @@
+#!/usr/bin/env node
+
+const fs = require('fs');
+const https = require('https');
+const { spawn } = require('child_process');
+
+const extension = process.platform === 'win32' ? '.exe' : '';
+const targetPath = `${__dirname}/bin/tauri-cli${extension}`;
+
+const args = process.argv || [];
+args.splice(0, 2);
+
+const main = async () => {
+    if (!fs.existsSync(targetPath)) {
+        const version = require('./package.json').version;
+        let platform = process.platform;
+
+        if (platform === 'win32') {
+            platform = 'windows';
+        } else if (platform === 'linux') {
+            platform = 'linux';
+        } else if (platform === 'darwin') {
+            platform = 'macos';
+        } else {
+            throw Error('Unsupported platform');
+        }
+
+        let url = `https://github.com/tauri-apps/binary-releases/releases/download/tauri-cli-v${version}/tauri-cli_${platform}${extension}`;
+
+        console.log('Downloading Rust CLI...');
+
+        fs.mkdirSync(`${__dirname}/bin`, { recursive: true });
+        const file = fs
+            .createWriteStream(targetPath)
+            .on('finish', () => {
+                file.close(() => {
+                    const child = spawn(targetPath, ['tauri', ...args], {
+                        cwd: process.cwd(),
+                        stdio: 'inherit',
+                    });
+
+                    child.on('close', code => {
+                        console.log(`child process exited with code ${code}`);
+                    });
+                });
+            })
+            .on('error', e => {
+                fs.unlink(targetPath, () => {});
+                throw e;
+            });
+
+        // Nested requests, as the first one is always a redirect.
+        https.get(url, res1 => {
+            https
+                .get(res1.headers.location || url, res => {
+                    if (res.statusCode === 200) {
+                        res.pipe(file);
+                    } else {
+                        file.close();
+                        fs.unlink(targetPath, () => {});
+                        throw `Server responded with ${res.statusCode}: ${res.statusMessage}`;
+                    }
+                })
+                .on('error', e => {
+                    file.close();
+                    fs.unlink(targetPath, () => {});
+                    throw e;
+                });
+        });
+    } else {
+        const child = spawn(targetPath, ['tauri', ...args], {
+            cwd: process.cwd(),
+            stdio: 'inherit',
+        });
+
+        child.on('close', code => {
+            console.log(`child process exited with code ${code}`);
+        });
+    }
+};
+
+main().catch(e => {
+    throw e;
+});
